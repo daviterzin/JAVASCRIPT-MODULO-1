77c02860c2bc254782e6402725128e9908ee736c
Matheus Davi <75388619+daviterzin@users.noreply.github.com>
2025-01-07 12:50:53 -0300

    Add files via upload

---
+++ JAVASCRIPT MODULO 1/AULA 10/index.js
@@ -0,0 +1,11 @@
+let nome = "Matheus" 
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert (nome)
+//nome = window.prompt ("insira seu nome completo")
+let confirmacao = window.confirm("confirme essa ação?")
+
+console.log(confirmacao)
---
+++ JAVASCRIPT MODULO 1/exercicio 2/velocidade.js
@@ -0,0 +1,5 @@
+const veiculo1 = prompt("Informe o nome do primeiro veículo:")
+const velocidade1 = parseFloat(prompt("Informe a velocidade do primeiro veículo:"))
+
+const veiculo2 = prompt("Informe o nome do segundo veículo:")
+const velocidade2 = parseFloat(prompt("Informe a velocidade do segundo veículo:"))
